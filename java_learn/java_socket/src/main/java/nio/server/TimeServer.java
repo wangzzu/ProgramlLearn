0b706a86c4502d833cefa043eed2899f936fa5b6
wangzzu <wangm92@163.com>
2017-08-04 22:19:36 +0800

    java NIO

---
+++ java_learn/java_socket/src/main/java/nio/server/TimeServer.java
@@ -0,0 +1,21 @@
+package nio.server;
+
+import java.io.IOException;
+
+/**
+ * Created by matt on 02/08/2017.
+ */
+public class TimeServer {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
